cc22b7807af4135547dc75a43b9890e9fc0a63a2
kishi4web <75372859+kishi4web@users.noreply.github.com>
2022-01-26 21:02:56 +0900

    Update cmain.js

--- cmain.js
+++ cmain.js
@@ -2,11 +2,11 @@ function setCookie(cname,cvalue,exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
-  cvalue = cvalue+cvalue+cvalue;
+  cvalue = cvalue+cvalue;
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-setCookie("Web Technology b", "正解です", 3); 
+setCookie("Web Technology b", "正解!", 3); 
 
 
 
