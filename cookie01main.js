d81699bf911dbb17914ac8ca93109fb8711c8c72
kishi4web <75372859+kishi4web@users.noreply.github.com>
2022-01-26 20:27:21 +0900

    Update cookie01main.js

--- cookie01main.js
+++ cookie01main.js
@@ -1,12 +1,12 @@
-
 function setCookie(cname,cvalue,exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
+  cvalue = cvalue+cvalue+cvalue;
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-let para = document.getElementById('para');
-para.addEventListener('click', setCookie("b1","正解です",30);
+setCookie("Web Technology b", "正解です", 3); 
+
 
 
